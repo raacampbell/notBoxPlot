71177bd67d8cae41425baee41b2db25973945b0e
Rob Campbell <git@raacampbell.com>
2016-06-13 19:06:02 +0300

    Nicer examples

--- code/plotting/NBP_example.m
+++ code/plotting/NBP_example.m
@@ -1,27 +1,67 @@
 function NBP_example
 
 clf 
+W=0.45; %image width
 
-subplot(2,2,1)  
-r=randn(10,5);
+axes('position',[0.05,0.53,W,W])
+r=randn(40,5);
 for ii=1:5
   r(:,ii)=r(:,ii)+ii*0.3;
 end
-H=notBoxPlot(r,[],0.5);
+notBoxPlot(r,[],0.5);
+box on
+grid on
 
 
 
-subplot(2,2,2)
-r=randn(10,25);
-r(:,1:4:end)=0.75*r(:,1:4:end)+1.75;
-H=notBoxPlot(r);
+axes('position',[0.53,0.53,W,W])
+r=randn(20,20);
+
+IND=zeros(1,20);
+IND(1:4:size(r,2))=1;
+r(:,find(IND))=0.75*r(:,1:4:end)+1.75;
+
+H=notBoxPlot(r,[],0.6);
 d=[H.data];
-set(d(1:4:end),'markerfacecolor',[0.4,1,0.4],'color',[0,0.4,0])
+
+%Highlight the plots with higher means
+set(d(find(IND)),'markerfacecolor',[0.4,1,0.4],'color',[0,0.4,0])
+
+%higher means as green
+set([H(find(IND)).data],'MarkerSize',4,...
+	'markerFaceColor',[1,1,1]*0.25,...
+	'markerEdgeColor', 'none')
+set([H(find(IND)).semPtch],...
+	'FaceColor',[0,0.75,0],...
+	'EdgeColor','none')
+set([H(find(IND)).sdPtch],...
+	'FaceColor',[0.6,1,0.6],...
+	'EdgeColor','none')
+set([H(find(IND)).mu],...
+	'Color',[0,0.4,0])
+
+
 set(gca,'XTick',[]) 
-set([H.data],'MarkerSize',3,'LineWidth',0.5)
 
 
-subplot(2,2,3)
+%lower means as gray
+set([H(find(~IND)).data],'MarkerSize',4,...
+	'markerFaceColor',[1,1,1]*0.5,...
+	'markerEdgeColor', 'none')
+set([H(find(~IND)).semPtch],...
+	'FaceColor',[1,1,1]*0.25,...
+	'EdgeColor','none')
+set([H(find(~IND)).sdPtch],...
+	'FaceColor',[1,1,1]*0.75,...
+	'EdgeColor','none')
+set([H(find(~IND)).mu],...
+	'Color','b')
+
+box on
+
+
+
+axes('position',[0.05,0.05,W,W])
 x=[1,2,3,3];
 y=randn(20,length(x));
 y(:,end)=0.5*y(:,end)+4;
@@ -29,11 +69,16 @@ y(:,end-1)=y(:,end-1)-1;
 y(1:8,end-1:end)=nan;
 
 H=notBoxPlot(y,x,0.6,'sdline');
-set(H(end).data,'Marker','^','MarkerSize',5)
+set(H(end).data,'Marker','^',...
+	'MarkerSize',5)
+set([H.sd],'LineWidth',4)
+box on
+grid on
 
 
-subplot(2,2,4)
-H=notBoxPlot(randn(10,1)+7,5,[],'line');
+
+axes('position',[0.53,0.05,W,W])
+H=notBoxPlot(randn(10,1)+7,2,[],'line');
 set(H.data,'color','b','Marker','.') 
 
 r=randn(20,10);
@@ -42,12 +87,16 @@ for ii=1:10
 end
 
 H=notBoxPlot(r,[],0.5);
-set([H.data],'color',[1,1,1]*0.35,'marker','.')
+set([H.data],...
+	'MarkerFaceColor',[1,1,1]*0.35,....
+	'markerEdgeColor',[1,1,1]*0.35,...
+	'MarkerSize',3)
+
 set([H.mu],'color','w')
 J=jet(length(H));
 for ii=1:length(H)
   set(H(ii).sdPtch,'FaceColor',J(ii,:),...
-                   'EdgeColor',J(ii,:)*0.6)
+                   'EdgeColor','none')
 
   set(H(ii).semPtch,'FaceColor',J(ii,:)*0.3,...
                    'EdgeColor','none')
