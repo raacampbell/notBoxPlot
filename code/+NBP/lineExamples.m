febb6eb8e1ede64288845a7da911b4446ad4ceb1
Rob Campbell <git@raacampbell.com>
2017-09-25 13:07:57 +0200

    Illustrate how to plot different number of points in each nbp.

--- code/+NBP/lineExamples.m
+++ code/+NBP/lineExamples.m
@@ -1,22 +1,20 @@
 function lineExamples
 
-% % Mixing lines and areas [note that the way notBoxPlot
-% % sets the x axis limits can cause problems when combining plots
-% % this way]
+% % Mixing lines and areas and using different number of points
+% % in each group [note that the way notBoxPlot sets the x axis 
+% % limits can cause problems when combining plots this way]
 %
 % clf
 %
 % subplot(1,2,1)
 % h=notBoxPlot(randn(10,1)+4,5,'style','line');
-% set(h.data,'color','m')  
+% set(h.data,'color','m')
 % h=notBoxPlot(randn(50,10));
 % set(h(5).data,'color','m')
 %
 % subplot(1,2,2)
-% y=randn(50,1);
-% clf
-% notBoxPlot(y,1,'style','sdline')
-% notBoxPlot(y,2)   
+% notBoxPlot(randn(25,1),1,'style','sdline')
+% notBoxPlot(randn(50,1),2) % more points
 % xlim([0,3])
 
 help(['NBP.',mfilename])
@@ -30,7 +28,6 @@ h=notBoxPlot(randn(50,10));
 set(h(5).data,'color','m')
 
 subplot(1,2,2)
-y=randn(50,1);
-notBoxPlot(y,1,'style','sdline')
-notBoxPlot(y,2)   
+notBoxPlot(randn(25,1),1,'style','sdline')
+notBoxPlot(randn(50,1),2) % more points
 xlim([0,3])
